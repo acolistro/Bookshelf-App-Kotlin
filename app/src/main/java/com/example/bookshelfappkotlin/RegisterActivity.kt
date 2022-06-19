9c20fdd2675ca54d380d20884d081f43e60b7279
acolistro <arcolistro@gmail.com>
2022-06-19 05:16:58 -0700

    Add functionality to take in user info, validate it and send it to firebase in the register activity

--- app/src/main/java/com/example/bookshelfappkotlin/RegisterActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/RegisterActivity.kt
@@ -1,9 +1,14 @@
 package com.example.bookshelfappkotlin
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.example.bookshelfappkotlin.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -13,9 +18,124 @@ class RegisterActivity : AppCompatActivity() {
     //Firebase auth
     private lateinit var firebaseAuth: FirebaseAuth
 
+    //Progress dialogue
+    private lateinit var progressDialog: ProgressDialog
+
+    private var name = ""
+    private var email = ""
+    private var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //init progress dialogue, will show while creating account | Register user
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false) // goes away when user taps outside the area
+
+        //handle back button, go to previous screen
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
+
+        //handle click, begin registration
+        binding.registerBtn.setOnClickListener {
+            /*Steps
+            * 1) Input data
+            * 2) Validate data
+            * 3) Create account - Firebase Auth
+            * 4) Save user info - Firebase Realtime Database*/
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        //1) Input data
+        name = binding.nameEt.text.toString().trim()
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+        val cPassword = binding.cPasswordEt.text.toString().trim()
+
+        //2) Validate data
+        if (name.isEmpty()) {
+            //empty name...
+            Toast.makeText(this, "Enter your name...", Toast.LENGTH_SHORT).show()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //invalid email pattern
+            Toast.makeText(this, "Invalid email...", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()) {
+            //empty password
+            Toast.makeText(this, "Enter password...", Toast.LENGTH_SHORT).show()
+        } else if (cPassword.isEmpty()) {
+            //empty password
+            Toast.makeText(this, "Confirm password...", Toast.LENGTH_SHORT).show()
+        } else if (password != cPassword) {
+            Toast.makeText(this, "Password doesn't match...", Toast.LENGTH_SHORT).show()
+        } else {
+            createUserAccount()
+        }
+    }
+
+    private fun createUserAccount() {
+        //3) Create account - Firebase Auth
+        //show progress
+        progressDialog.setMessage("Creating Account...")
+        progressDialog.show()
+
+        //Create user in Firebase Auth
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //account created, now add user info in db
+                updateUserInfo()
+
+            }
+            .addOnFailureListener { e ->
+                //account creation failed
+                progressDialog.dismiss()
+                Toast.makeText(this, "Account creation failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun updateUserInfo() {
+        //4) Save user info - firebase realtime database
+        progressDialog.setMessage("Saving user info...")
+
+        //timestamp
+        val timestamp = System.currentTimeMillis()
+
+        //get user uid now that it has been created
+        val uid = firebaseAuth.uid
+
+        //set up data to add in db
+        val hashMap: HashMap<String, Any?> = HashMap()
+        hashMap["uid"] = uid
+        hashMap["email"] = email
+        hashMap["name"] = name
+        hashMap["profileImage"] = "" //add empty, will do in profile edit
+        hashMap["userType"] = "user" //possible values are user/admin, will change value admin manually on firebase db
+        hashMap["timestamp"] = timestamp
+
+        //set data to db
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(uid!!)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                //user info saved, open user dashboard
+                progressDialog.dismiss()
+                Toast.makeText(this, "Account created...", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@RegisterActivity, DashboardUserActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e ->
+                //failed adding data to db
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed saving user info due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
