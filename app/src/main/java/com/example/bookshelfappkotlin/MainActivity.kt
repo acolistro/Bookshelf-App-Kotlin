42ff4e1dbeb57014d4f5c05ebe58033b2fa912d3
acolistro <arcolistro@gmail.com>
2022-06-18 22:57:15 -0700

    Add binding and click listeners to main activity

--- app/src/main/java/com/example/bookshelfappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.bookshelfappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookshelfappkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //View binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Handle click, login
+        binding.loginBtn.setOnClickListener {
+            //tbd
+        }
+
+        binding.skipBtn.setOnClickListener {
+            //tbd
+        }
+
+
     }
 }
