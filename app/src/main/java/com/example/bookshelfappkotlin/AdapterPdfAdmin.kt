95bdb50da3036f79f5f9af3fdd4b63e13e961e78
acolistro <arcolistro@gmail.com>
2022-06-28 02:09:56 -0700

    Add initial code for pdf admin adapter and pdf model

---
+++ app/src/main/java/com/example/bookshelfappkotlin/AdapterPdfAdmin.kt
@@ -0,0 +1,65 @@
+package com.example.bookshelfappkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookshelfappkotlin.databinding.RowPdfAdminBinding
+import com.google.firebase.database.core.Context
+
+class AdapterPdfAdmin : RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin> {
+
+    //context
+    private var context: android.content.Context
+
+    //arrayList to hold pdfs
+    private var pdfArrayList: ArrayList<ModelPdf>
+
+    //view binding
+    private lateinit var binding: RowPdfAdminBinding
+
+    //constructor
+    constructor(context: android.content.Context, pdfArrayList: ArrayList<ModelPdf>) : super() {
+        this.context = context
+        this.pdfArrayList = pdfArrayList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderPdfAdmin {
+        //bind/inflate layout row_pdf_admin.xml
+        binding = RowPdfAdminBinding.inflate(LayoutInflater.from(context), parent, false)
+        return HolderPdfAdmin(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: HolderPdfAdmin, position: Int) {
+        /*Get data, set data, handle click etc*/
+        //get data
+        val model = pdfArrayList[position]
+        val pdfId = model.id
+        val categoryId = model.categoryId
+        val title = model.title
+        val description = model.description
+        val pdfUrl = model.url
+        val timestamp = model.timestamp
+        //convert timestamp to dd/mm/yyyy format
+
+        
+    }
+
+    override fun getItemCount(): Int {
+        return pdfArrayList.size //items count
+    }
+
+    /*View Holder class for row_pdf_admin.xml*/
+    inner class HolderPdfAdmin(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //UI Views of row_pdf_admin.xml
+        val pdfView = binding.pdfView
+        val progressBar = binding.progressBar
+        val titleTv = binding.titleTv
+        val descriptionTv = binding.descriptionTv
+        val categoryTv = binding.categoryTv
+        val sizeTv = binding.sizeTv
+        val dateTv = binding.dateTv
+        val moreBtn = binding.moreBtn
+    }
+
+}
