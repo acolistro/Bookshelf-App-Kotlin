03cb2068c3c5429a794dceac6f27273251c0c2d4
acolistro <arcolistro@gmail.com>
2022-07-01 01:47:54 -0700

    Small fixes and edits to MyApplication

--- app/src/main/java/com/example/bookshelfappkotlin/MyApplication.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/MyApplication.kt
@@ -48,17 +48,16 @@ class MyApplication: Application() {
                     val kb = bytes/1024
                     val mb = kb/1024
                     if (mb>1) {
-                        sizeTv.text = "${String.format("$.2f", mb)} MB"
+                        sizeTv.text = "${String.format("%.2f", mb)} MB"
                     } else if (kb>=1) {
-                        sizeTv.text = "${String.format("$.2f", kb)} KB"
+                        sizeTv.text = "${String.format("%.2f", kb)} KB"
                     } else {
-                        sizeTv.text = "${String.format("$.2f", bytes)} bytes"
+                        sizeTv.text = "${String.format("%.2f", bytes)} bytes"
                     }
                 }
                 .addOnFailureListener { e ->
                 //failed to get metadata
                 Log.d(TAG, "loadPdfSize: Failed to get metadata due to ${e.message}")
-
                 }
         }
 
@@ -90,6 +89,7 @@ class MyApplication: Application() {
                             Log.d(TAG, "loadPdfFromUrlSinglePage: ${t.message}")
                         }
                         .onLoad { nbPages ->
+                            Log.d(TAG, "loadPdfFromUrlSinglePage: Pages: $nbPages")
                             //pdf loaded, we can set page count, pdf thumbnail
                             progressBar.visibility = View.INVISIBLE
 
@@ -98,6 +98,7 @@ class MyApplication: Application() {
                                 pagesTv.text = "$nbPages"
                             }
                         }
+                        .load()
                 }
                 .addOnFailureListener { e ->
                     //failed to get metadata
