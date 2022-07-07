5876669fa8317bedfffabb9ade04d3dbbc5a751e
acolistro <arcolistro@gmail.com>
2022-07-07 07:45:46 -0700

    Add xml layout elements for pdf detail screen

---
+++ app/src/main/java/com/example/bookshelfappkotlin/activity/PdfDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookshelfappkotlin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookshelfappkotlin.R
+
+class PdfDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_detail)
+    }
+}
