4757f70713730cb3227f8c60ada308f1401b8a8c
acolistro <arcolistro@gmail.com>
2022-07-12 12:55:15 -0700

    Add code to save pdf to downloads folder, account for permissions, on button click

--- app/src/main/java/com/example/bookshelfappkotlin/activity/PdfDetailActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/activity/PdfDetailActivity.kt
@@ -1,8 +1,17 @@
 package com.example.bookshelfappkotlin.activity
 
+import android.app.DownloadManager
+import android.app.ProgressDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.bookshelfappkotlin.Constants
 import com.example.bookshelfappkotlin.MyApplication
 import com.example.bookshelfappkotlin.MyApplication.Companion.incrementBookViewCount
 import com.example.bookshelfappkotlin.databinding.ActivityPdfDetailBinding
@@ -10,14 +19,28 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class PdfDetailActivity : AppCompatActivity() {
 
     //view binding
     private lateinit var binding: ActivityPdfDetailBinding
 
-    //book id
+    private companion object {
+        //TAG
+        const val TAG = "BOOK_DETAILS_TAG"
+    }
+
+    //book id, get from intent
     private var bookId = ""
+    //get from firebase
+    private var bookTitle = ""
+    private var bookUrl = ""
+
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +49,11 @@ class PdfDetailActivity : AppCompatActivity() {
 
         //get book id from intent
         bookId = intent.getStringExtra("bookId")!!
+
+        //init progress bar
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait...")
+        progressDialog.setCanceledOnTouchOutside(false)
 
         //increment book view count whenever this screen starts
         incrementBookViewCount(bookId)
@@ -44,6 +72,79 @@ class PdfDetailActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //handle click, download book/pdf
+        binding.downloadBookBtn.setOnClickListener {
+            //Check WRITE_EXTERNAL_STORAGE permission first, if granted download book, if not granted request permission
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "onCreate: STORAGE PERMISSION is already granted")
+                downloadBook()
+            } else {
+                Log.d(TAG, "onCreate: STORAGE PERMISSION was not granted, must be requested")
+                requestStoragePermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private val requestStoragePermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted:Boolean ->
+        //check if granted or not
+        if (isGranted) {
+            Log.d(TAG, "onCreate: STORAGE PERMISSION is already granted")
+            downloadBook()
+        } else {
+            Log.d(TAG, "onCreate: STORAGE PERMISSION is denied")
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun downloadBook() {
+        Log.d(TAG, "downloadBook: Downloading book...")
+        progressDialog.setMessage("Downloading Book")
+        progressDialog.show()
+
+        //download book from firebase storage using url
+        val storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(bookUrl)
+        storageReference.getBytes(Constants.MAX_BYTES_PDF)
+            .addOnSuccessListener { bytes ->
+                Log.d(TAG, "downloadBook: Book downloaded...")
+                saveToDownloadsFolder(bytes)
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Log.d(TAG, "downloadBook: Failed to download book due to ${e.message}")
+                Toast.makeText(this, "Failed to download book due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveToDownloadsFolder(bytes: ByteArray) {
+        Log.d(TAG, "saveToDownloadsFolder: saving downloaded book...")
+
+        val nameWithExtension = "$bookTitle"
+
+        try {
+            val downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            downloadsFolder.mkdirs() //create folder if it doesn't exist
+
+            val filePath = downloadsFolder.path + "/" + nameWithExtension
+
+            val out = FileOutputStream(filePath)
+            out.write(bytes)
+            out.close()
+
+            Toast.makeText(this, "Saved to Downloads Folder", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "saveToDownloadsFolder: Saved to Downloads Folder")
+            progressDialog.dismiss()
+            incrementDownloadCount()
+        }
+        catch (e: Exception) {
+            progressDialog.dismiss()
+            Log.d(TAG, "saveToDownloadsFolder: failed to save due to ${e.message}")
+            Toast.makeText(this, "Failed to save book due to ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun incrementDownloadCount() {
+        //increment downloads count to firebase
     }
 
     private fun loadBookDetails() {
@@ -57,9 +158,9 @@ class PdfDetailActivity : AppCompatActivity() {
                     val description = "${snapshot.child("description").value}"
                     val downloadsCount = "${snapshot.child("downloadsCount").value}"
                     val timestamp = "${snapshot.child("timestamp").value}"
-                    val title = "${snapshot.child("title").value}"
+                    bookTitle = "${snapshot.child("title").value}"
                     val uid = "${snapshot.child("uid").value}"
-                    val url = "${snapshot.child("url").value}"
+                    bookUrl = "${snapshot.child("url").value}"
                     val viewsCount = "${snapshot.child("viewsCount").value}"
 
                     //format date
@@ -68,12 +169,12 @@ class PdfDetailActivity : AppCompatActivity() {
                     //load pdf category
                     MyApplication.loadCategory(categoryId, binding.categoryTv)
                     //load pdf thumbnail, pages count
-                    MyApplication.loadPdfFromUrlSinglePage("$url", "$title", binding.pdfView, binding.progressBar, binding.pagesTv)
+                    MyApplication.loadPdfFromUrlSinglePage("$bookUrl", "$bookTitle", binding.pdfView, binding.progressBar, binding.pagesTv)
                     //load pdf size
-                    MyApplication.loadPdfSize("$url", "$title", binding.sizeTv)
+                    MyApplication.loadPdfSize("$bookUrl", "$bookTitle", binding.sizeTv)
 
                     //set data
-                    binding.titleTv.text = title
+                    binding.titleTv.text = bookTitle
                     binding.descriptionTv.text = description
                     binding.viewsTv.text = viewsCount
                     binding.downloadsTv.text = downloadsCount
