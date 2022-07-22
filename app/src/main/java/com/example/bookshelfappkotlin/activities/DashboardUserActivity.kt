1dc1fcfa8309cc2bd51cef8b2b615dcd7812b874
acolistro <arcolistro@gmail.com>
2022-07-22 15:58:46 -0700

    Update user dash activity to user fragment to show filtered list when user clicks a tab

--- app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardUserActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardUserActivity.kt
@@ -1,10 +1,21 @@
 package com.example.bookshelfappkotlin.activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.bookshelfappkotlin.databinding.ActivityDashboardUserBinding
+import com.example.bookshelfappkotlin.fragments.BookUserFragment
+import com.example.bookshelfappkotlin.model.ModelCategory
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashboardUserActivity : AppCompatActivity() {
     //View binding
@@ -12,6 +23,9 @@ class DashboardUserActivity : AppCompatActivity() {
 
     //Firebase Auth
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +36,125 @@ class DashboardUserActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
 
+        setupWithViewPagerAdapter(binding.viewPager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
         //handle click, logout
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+    }
+
+    private fun setupWithViewPagerAdapter(viewPager: ViewPager) {
+        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+        this
+        )
+
+        //init list
+        categoryArrayList = ArrayList()
+
+        //load categories from DB
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var ts1 = 1
+                //clear list
+                categoryArrayList.clear()
+
+                /*Load some static categories e.g. All, Most Viewed, Most Downloaded*/
+                //Add data to module
+                val modelAll = ModelCategory("01", "All", ts1.toString(), "")
+                val modelMostViewed = ModelCategory("01", "Most Viewed", ts1.toString(), "")
+                val modelMostDownloaded = ModelCategory("01", "Most Downloaded", ts1.toString(), "")
+                //add to list
+                categoryArrayList.add(modelAll)
+                categoryArrayList.add(modelMostViewed)
+                categoryArrayList.add(modelMostDownloaded)
+                //add to viewPagerAdapter
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelAll.id}",
+                        "${modelAll.category}",
+                        "${modelAll.uid}"
+                    ), modelAll.category
+                )
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelMostViewed.id}",
+                        "${modelMostViewed.category}",
+                        "${modelMostViewed.uid}"
+                    ), modelMostViewed.category
+                )
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelMostDownloaded.id}",
+                        "${modelMostDownloaded.category}",
+                        "${modelMostDownloaded.uid}"
+                    ), modelMostDownloaded.category
+                )
+                //refresh list
+                viewPagerAdapter.notifyDataSetChanged()
+
+                //NOW load from firebase db
+                for (ds in snapshot.children) {
+                    //get data in a model
+                    val model = ds.getValue(ModelCategory::class.java)
+                    //add to list
+                    categoryArrayList.add(model!!)
+                    //add to viewPagerAdapter
+                    viewPagerAdapter.addFragment(
+                        BookUserFragment.newInstance(
+                            "${model.id}",
+                            "${model.category}",
+                            "${model.uid}"
+                        ), model.category
+                    )
+                    //refresh list
+                    viewPagerAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        //set up adapter to viewpager
+        viewPager.adapter = viewPagerAdapter
+    }
+
+    class ViewPagerAdapter(fm: FragmentManager, behavior: Int, context: Context): FragmentPagerAdapter(fm, behavior) {
+        //hold list of fragments i.e. new instances of same fragment for each category
+        private val fragmentsList: ArrayList<BookUserFragment> = ArrayList()
+        //list of titles of categories, for tabs
+        private val fragmentTitleList: ArrayList<String> = ArrayList()
+
+        private val context: Context
+
+        init {
+            this.context = context
+        }
+
+        override fun getCount(): Int {
+            return fragmentsList.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentsList[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return fragmentTitleList[position]
+        }
+
+        public fun addFragment(fragment: BookUserFragment, title: String) {
+            //add fragment that will be passed as parameter in fragmentList
+            fragmentsList.add(fragment)
+            //add title that will be passed as parameter
+            fragmentTitleList.add(title)
         }
     }
 
