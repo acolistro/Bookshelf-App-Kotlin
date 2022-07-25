4b8aacc49876120fbe49d4e3fecd0960a46380d7
acolistro <arcolistro@gmail.com>
2022-07-24 20:43:24 -0700

    Add profile activity and show/hide profile and logout buttons depending on whether user is logged in

--- app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardAdminActivity.kt
@@ -69,6 +69,11 @@ class DashboardAdminActivity : AppCompatActivity() {
         binding.addPdfFab.setOnClickListener {
             startActivity(Intent(this, PdfAddActivity::class.java))
         }
+
+        //handle click, open profile
+        binding.profileBtn.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
     }
 
     private fun loadCategories() {
--- app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardUserActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/activities/DashboardUserActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -44,6 +45,11 @@ class DashboardUserActivity : AppCompatActivity() {
             firebaseAuth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+
+        //handle click, open profile
+        binding.profileBtn.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
         }
     }
 
@@ -158,17 +164,26 @@ class DashboardUserActivity : AppCompatActivity() {
         }
     }
 
+    /*This activity can be opened with or without login, so hide logout and profile button when user isn't logged in*/
     private fun checkUser() {
         //get current user
         val firebaseUser = firebaseAuth.currentUser
         if (firebaseUser == null) {
             //not logged in, user can stay in user dashboard without login too
             binding.subTitleTv.text = "Not Logged In"
+
+            //hide logout/profile buttons
+            binding.profileBtn.visibility = View.GONE
+            binding.logoutBtn.visibility = View.GONE
         } else {
             //logged in, get and show user info
             val email = firebaseUser.email
             //set to textview of toolbar
             binding.subTitleTv.text = email
+
+            //show logout/profile buttons
+            binding.profileBtn.visibility = View.VISIBLE
+            binding.logoutBtn.visibility = View.VISIBLE
         }
     }
 }
