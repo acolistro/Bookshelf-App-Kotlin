9b09c118893d34f5e10121785898c11ed0630b06
acolistro <arcolistro@gmail.com>
2022-07-31 19:27:38 -0700

    Create pdf favorite adapter and add loadBookDetails function

---
+++ app/src/main/java/com/example/bookshelfappkotlin/adapters/AdapterPdfFavorite.kt
@@ -0,0 +1,97 @@
+package com.example.bookshelfappkotlin.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookshelfappkotlin.MyApplication
+import com.example.bookshelfappkotlin.databinding.RowPdfFavoriteBinding
+import com.example.bookshelfappkotlin.models.ModelPdf
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class AdapterPdfFavorite : RecyclerView.Adapter<AdapterPdfFavorite.HolderPdfFavorite> {
+
+    //Context
+    private val context: Context
+    //Arraylist to hold books
+    private var booksArrayList: ArrayList<ModelPdf>
+    //view binding
+    private lateinit var binding: RowPdfFavoriteBinding
+    //constructor
+    constructor(context: Context, booksArrayList: ArrayList<ModelPdf>) {
+        this.context = context
+        this.booksArrayList = booksArrayList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderPdfFavorite {
+        //bind/inflate row_pdf_favorite.xml
+        binding = RowPdfFavoriteBinding.inflate(LayoutInflater.from(context), parent, false)
+
+        return HolderPdfFavorite(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: HolderPdfFavorite, position: Int) {
+        /*-----Get data, set data, handle clicks etc----*/
+
+        //get data; from [Users > uid > Favorites] we will only have ids of favorite books so we have to load their details from [Books] node
+        val model = booksArrayList[position]
+
+        loadBookDetails(model, holder)
+    }
+
+    private fun loadBookDetails(model: ModelPdf, holder: AdapterPdfFavorite.HolderPdfFavorite) {
+        val bookId = model.id
+
+        val ref = FirebaseDatabase.getInstance().getReference("Books")
+        ref.child(bookId)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //get book info
+                    val categoryId = "${snapshot.child("categoryId").value}"
+                    val description = "${snapshot.child("description").value}"
+                    val downloadsCount = "${snapshot.child("downloadsCount").value}"
+                    val timestamp = "${snapshot.child("timestamp").value}"
+                    val title = "${snapshot.child("title").value}"
+                    val uid = "${snapshot.child("uid").value}"
+                    val url = "${snapshot.child("url").value}"
+                    val viewsCount = "${snapshot.child("viewsCount").value}"
+
+                    //format date
+                    val date = MyApplication.formatTimeStamp(timestamp)
+
+                    MyApplication.loadCategory("$categoryId", holder.categoryTv)
+                    MyApplication.loadPdfFromUrlSinglePage("$url", "$title", holder.pdfView, holder.progressBar, null)
+                    MyApplication.loadPdfSize("$url", "$title", holder.sizeTv)
+
+                    holder.titleTv.text = title
+                    holder.descriptionTv.text = description
+                    holder.dateTv.text = date
+                }
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+    }
+
+    override fun getItemCount(): Int {
+        return booksArrayList.size //return size of list | number of items in list
+    }
+
+    /*ViewHolder class to manage UI views of row_pdf_favorite.xml*/
+    inner class HolderPdfFavorite(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //init UI Views
+        var pdfView = binding.pdfView
+        var progressBar = binding.progressBar
+        var titleTv = binding.titleTv
+        var removeFavBtn = binding.removeFavBtn
+        var descriptionTv = binding.descriptionTv
+        var categoryTv = binding.categoryTv
+        var sizeTv = binding.sizeTv
+        var dateTv = binding.dateTv
+    }
+
+}
