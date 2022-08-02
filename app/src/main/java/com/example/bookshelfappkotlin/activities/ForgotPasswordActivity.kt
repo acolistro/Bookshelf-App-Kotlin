bc6442da7f0dd485b246377947f54b26dcbd20f2
acolistro <arcolistro@gmail.com>
2022-08-02 03:32:54 -0700

    Remove unused imports

--- app/src/main/java/com/example/bookshelfappkotlin/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/bookshelfappkotlin/activities/ForgotPasswordActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
-import com.example.bookshelfappkotlin.R
 import com.example.bookshelfappkotlin.databinding.ActivityForgotPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 
